dc85df375d667b1770be1adca87248c5f44d39c6
Prem <prem.nagalla@gmail.com>
2013-04-30 19:42:14 +0530

    Updated asset loading to load only basic common files along with current controller files, through out the application

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// Atleast empty file is neccessary to avoid errors in Production.
