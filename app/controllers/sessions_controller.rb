68580513b3ea9351d9db96c5f177f89f905cbe63
Jyothi <jyothis.kalangi@gmail.com>
2013-05-06 15:18:44 +0530

    User is able to login with username or email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,20 @@ class SessionsController < ApplicationController
     if params[:provider]=="facebook"
       user = User.from_omniauth(env["omniauth.auth"])
     else
-     user = User.authenticate(params[:email], params[:password]) 
+     user = User.authenticate(params[:login], params[:password]) 
    end
-    if user
+
+    if user == "Login with facebook"
+      flash.now.alert = "Login with Facebook"
+      render "new"
+    elsif user
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
     end
+
   end
 
   def destroy
