bcf848da2ad68eeb6fe688fe840b0c91875a87b6
Jyothi <jyothis.kalangi@gmail.com>
2013-05-06 15:22:51 +0530

    Added username and missing validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  # Fields Declararion
   field :email, type: String
   field :provider, type: String
   field :uid, type: Integer
@@ -10,25 +13,34 @@ class User
   field :password_hash, type: String
   field :password_salt, type: String
   
-  attr_accessible :email, :password, :password_confirmation
-  
   attr_accessor :password
+  attr_accessible :email, :password, :password_confirmation, :username
+  
   before_save :encrypt_password
 
+  # VALIDATIONS
+  validates :username, :presence => true
   validates :email, :presence => true,
                     :uniqueness => { :case_sensitive => false }
-  validates :password, :presence => true, :on => :create
-  validates_confirmation_of :password
+  # validates :password, :presence => true, :on => :create
+  # validates_confirmation_of :password
   
-  def self.authenticate(email, password)
-    user = User.where(:email=>email).first
-    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-      user
-    else
-      nil
+  # CLASS METHODS
+  def self.authenticate(login, password)
+    facebook_user= User.where(:provider=> "facebook").and(:email =>login).first || User.where(:provider=> "facebook").and(:email=>login).first
+    if facebook_user
+      user = "Login with facebook"
+    else 
+    user = User.where(:username=>login.downcase).first || User.where(:email=>login).first
+      if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+        user
+      else
+        nil
+      end
     end
   end
   
+  # INSTANCE METHODS
   def encrypt_password
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
@@ -42,7 +54,7 @@ class User
         user.provider = auth.provider
         user.uid = auth.uid
         user.name = auth.info.name
-        user.username = auth.info.username
+        user.username = auth.extra.raw_info.username
         user.email = auth.info.email
         user.oauth_token = auth.credentials.token
         user.oauth_expires_at = Time.at(auth.credentials.expires_at)
