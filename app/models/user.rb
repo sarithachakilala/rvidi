eada50f752cddd43c7ebb82cc84274ee8082b944
Prem <prem.nagalla@gmail.com>
2013-05-06 09:39:00 +0530

    Updated models to fix small errors and added timestamps to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,24 @@
 class User
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  # Fields Declararion
   field :email, type: String
   field :password_hash, type: String
   field :password_salt, type: String
   
+  attr_accessor :password
   attr_accessible :email, :password, :password_confirmation
   
-  attr_accessor :password
   before_save :encrypt_password
-  
-  validates :email, :presence => true
+
+  # VALIDATIONS
+  validates :email, :presence => true,
                     :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true, :on => :create
   validates_confirmation_of :password
   
+  # CLASS METHODS
   def self.authenticate(email, password)
     user = User.where(:email=>email).first
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
@@ -23,6 +28,7 @@ class User
     end
   end
   
+  # INSTANCE METHODS
   def encrypt_password
     if password.present?
       self.password_salt = BCrypt::Engine.generate_salt
