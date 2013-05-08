2c278aeb35f254d83709c5faee4dcc4cef86e8fa
Prem <prem.nagalla@gmail.com>
2013-05-08 19:14:23 +0530

    Commented twitter configurations as required gem was not added and twitter integration work is in progress

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,8 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, configatron.facebook_app_id, configatron.facebook_app_secret
-  provider :twitter, configatron.twitter_consumer_key, configatron.twitter_consumer_secret
+  # Commenting, as returns error with out "omniauth-twitter" gem
+  # provider :twitter, configatron.twitter_consumer_key, configatron.twitter_consumer_secret
 end
 
 OmniAuth.config.on_failure = UsersController.action(:oauth_failure)
