f278ef93033f4ce178cf007f27f282d72b9f5900
Prem <prem.nagalla@gmail.com>
2013-05-14 19:24:01 +0530

    Updated User edit profile page and redirecting user to the same page after Saving the profile changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to profile_user_path(@user), notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
