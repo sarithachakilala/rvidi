511f04d65f0a145388a5f9587d7cf6db50fed791
Prem <prem.nagalla@gmail.com>
2013-05-28 16:41:26 +0530

    Integrated Application home page and Updated Comments displaying in Show details Page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
 
   def index
+    @all_shows = Show.all
+    
     respond_to do |format|
       format.html{}
       format.json { render json: user }
@@ -8,7 +10,6 @@ class HomeController < ApplicationController
   end
 
   def terms_condition
-   
   end
 
 end
