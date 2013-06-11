9fb47bac0924264b4408b5a75e55159668f95f28
Prem <prem.nagalla@gmail.com>
2013-06-11 22:01:55 +0530

    Integrated Video recording in new show page as well

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -48,17 +48,23 @@ class ShowsController < ApplicationController
     if @cameo.video_file.present?
       media_entry = Cameo.upload_video_to_kaltura(@cameo.video_file, session[:client], session[:ks])
       @cameo.set_uploaded_video_details(media_entry)
+    elsif @cameo.recorded_file.present?
+      media_entry = Cameo.upload_video_to_kaltura(@cameo.recorded_file, session[:client], session[:ks])
+      @cameo.set_uploaded_video_details(media_entry)
     else
       @show.cameos=[]
     end
+    @success = @show.save
 
     respond_to do |format|
-      if @show.save
+      if @success
         format.html { redirect_to @show, notice: 'Show was successfully created.' }
+        format.js {}
         format.json { render json: @show, status: :created, location: @show }
       else
         p "%"*80; p "errors while saving show ------------ : #{@show.errors}"
         format.html { render action: "new" }
+        format.js {}
         format.json { render json: @show.errors, status: :unprocessable_entity }
       end
     end
