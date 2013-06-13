60e94ba1f8b4e855162c714c31e41267c8060d97
vijayajyothi <jyothis.kalangi@gmail.com>
2013-06-13 13:43:26 +0530

    Most commented, small changes in UI

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
   def index
     @all_shows = Show.all
     @latest_show =  Show.limit(6).order('created_at desc')
+    @most_viewed =  Show.order('number_of_views desc')
+    @most_commented = Show.select("shows.*, count('comments.id') as comm_count").joins(:comments).group("shows.id").order("comm_count desc")
+    # @most_commented = Comment.count(:group =>"comments.show_id", :order=> "count_all desc")
     respond_to do |format|
       format.html{}
       format.json { render json: user }
