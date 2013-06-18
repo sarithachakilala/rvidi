ff20e4434374c986ac1f0606cef586e8f113332a
vijayajyothi <jyothis.kalangi@gmail.com>
2013-06-18 15:26:34 +0530

    Small changes regarding carousel

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -126,7 +126,7 @@ class ShowsController < ApplicationController
       @checkd_users.each do |each_friend|
         @user = User.find(each_friend) 
         InviteFriend.create(:director_id=> @show.user_id, :show_id=> @show.id, :contributor_id=>@user.id, :status =>"invited" )
-        notification = Notification.new(:show_id => @show.id, :from_id=>current_user.id, :to_id=> @user.id, :status => "contribute", :content=>" has Requested you to contribute for his Show")
+        notification = Notification.new(:show_id => @show.id, :from_id=>current_user.id, :to_id=> @user.id, :status => "contribute", :content=>" has Requested you to contribute for their Show ")
         notification.save!
       end
     end
@@ -138,7 +138,7 @@ class ShowsController < ApplicationController
     @user = User.find(params[:email_from])
     RvidiMailer.delay.invite_friend_toshow(params[:email], @user, @show.id)
     InviteFriend.create(:director_id=> @show.user_id, :show_id=> @show.id, :contributor_id=>@user.id, :status =>"invited" )
-    notification = Notification.new(:show_id => @show.id, :from_id=>current_user.id, :to_id=> '', :status => "contribute", :content=>" has Requested you to contribute for his Show", :to_email=>params[:email])
+    notification = Notification.new(:show_id => @show.id, :from_id=>current_user.id, :to_id=> '', :status => "contribute", :content=>" has Requested you to contribute for their Show ", :to_email=>params[:email])
     notification.save!
     redirect_to edit_show_path(:id=>@show.id)
   end
