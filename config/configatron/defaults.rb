b09df62c4d75d8686dd889ef437359a99bcb2bb9
Prem <prem.nagalla@gmail.com>
2013-06-20 16:39:44 +0530

    Updated cameos to save in kaltura server and added class method to add cameos from console or from rake task as well

--- config/configatron/defaults.rb
+++ config/configatron/defaults.rb
@@ -1,17 +1,29 @@
-# KALTUR Configurations
+# KALTUR Configurations (New1 for Kaltura Qwinix Server)
 # Account Registration Details
 configatron.kaltura_email = "pnagalla@qwinixtech.com"
-configatron.kaltura_password = "password@123"
-configatron.kaltura_partner_id = 1409052
-configatron.kaltura_sub_partner_id = 140905200
-configatron.administrator_secret = '0d625a8aba3cf91881edda68f0c5788e'
-configatron.user_secret = '80f9e5b7b61ee2f674bd319772784778'
-configatron.service_url = 'http://www.kaltura.com'
+configatron.kaltura_password = "Qwinix123"
+configatron.kaltura_partner_id = 102
+configatron.kaltura_sub_partner_id = 10200
+configatron.administrator_secret = '7ddc2a84ba3d581b8432d46df88699ab'
+configatron.user_secret = '5f7f8936e706856a103e80dbcc95a1cb'
+configatron.service_url = 'http://kaltura.qwinixtech.com/'
 configatron.time_out = 60
 configatron.user_name = 'pnagalla@qwinixtech.com'
 configatron.user_id = 'pnagalla@qwinixtech.com'
 
-# ########## ANOTHER ACCOUNT Registration Details
+# ########## ANOTHER ACCOUNT Registration Details (Old1 for Kaltura SASS)
+# configatron.kaltura_email = "pnagalla@qwinixtech.com"
+# configatron.kaltura_password = "password@123"
+# configatron.kaltura_partner_id = 1409052
+# configatron.kaltura_sub_partner_id = 140905200
+# configatron.administrator_secret = '0d625a8aba3cf91881edda68f0c5788e'
+# configatron.user_secret = '80f9e5b7b61ee2f674bd319772784778'
+# configatron.service_url = 'http://www.kaltura.com'
+# configatron.time_out = 60
+# configatron.user_name = 'pnagalla@qwinixtech.com'
+# configatron.user_id = 'pnagalla@qwinixtech.com'
+
+# ########## ANOTHER ACCOUNT Registration Details (Old2 for Kaltura SASS)
 # configatron.kaltura_email = "prem.nagalla@gmail.com"
 # configatron.kaltura_password = "password@123"
 # configatron.kaltura_partner_id = 1441251
