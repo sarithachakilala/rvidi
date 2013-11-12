900cc36b21cf19ef073b9156ca8bffbd62173654
Jose Antonio Pio Gil <josetonyp@latizana.com>
2013-11-12 14:13:52 +0530

    Deleting unused test and failing tests

--- spec/factories/documentations.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :documentation do
-    name "MyString"
-    description "MyString"
-  end
-end
