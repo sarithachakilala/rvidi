8a4309b071f4c3a7938174b1f98f8eafca035f28
Jose Antonio Pio Gil <josetonyp@latizana.com>
2013-11-15 17:47:54 +0530

    changing token id

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rvidi::Application.config.secret_token = 'dc5c5c740e41a6579a77310cfc3a9fc664f2fb3b1e606836ae1d9628429f272a93078de0a1ee2399b11d8fe0bcec34830a027f88db0fa806fcbaece86de143fe'
+Rvidi::Application.config.secret_token = 'dc5c5c740e41a6579a77310cfc3a9fc664f2fb3b1e606836ae1d9628429f272a93078de0a1ee2399b11d8fe0bcec34830a027f88db0fa806fcbaece86de143erhdfg'
