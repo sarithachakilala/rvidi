bfb37687236adb3b897d808cada46207e45cb58e
Raghavendra Joshi <raghugjoshi123@gmail.com>
2013-11-18 21:14:35 +0530

    code is modified

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -11,11 +11,6 @@ set :application, 'rvidi.qwinixtech.com'
 
 set :deploy_to, "#{base_path}/#{app_name}"
 
-set :default_environment, {
-  'PATH' => "#{deploy_to}/bin:$PATH",
-  'GEM_HOME' => "#{deploy_to}/gems"
-}
-
 set :branch, 'master'
 #set :branch, 'video-quality'
 #set :port, 1122
